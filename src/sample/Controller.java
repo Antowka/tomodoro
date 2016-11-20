7c0ca0a18f2feac33558fd2e53dfd462c7e7f880
Anton Nikanorov <anton@antowka.ru>
2016-11-20 14:28:56 +0300

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,89 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
+
+public class Controller {
+
+    private Stage primaryStage;
+
+    @FXML
+    private TextField stringTimer;
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private Button btnPause;
+
+    @FXML
+    private Button btnReset;
+
+    private Timer timer;
+
+    private final int timePeriod = 25 * 60;
+    private Integer currentTimer = timePeriod;
+    private final int timeTick = 1*1000;
+
+    public void initialize(Stage primaryStage1) {
+
+        this.primaryStage = primaryStage1;
+        stringTimer.setText(timeToString());
+        btnStart.setOnAction((event) -> onClickStart());
+        btnPause.setOnAction((event) -> onClickPause());
+        btnReset.setOnAction((event) -> onClickReset());
+
+        timer = new Timer(timeTick, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentTimer--;
+                stringTimer.setText(timeToString());
+
+                if(currentTimer == 0) {
+
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            primaryStage.requestFocus();
+                        }
+                    });
+
+                    timer.stop();
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        });
+    }
+
+    private void onClickStart() {
+        timer.start();
+    }
+
+    private void onClickPause() {
+        timer.stop();
+    }
+
+    private void onClickReset() {
+        timer.stop();
+        currentTimer = timePeriod;
+        stringTimer.setText(timeToString());
+    }
+
+    private String timeToString() {
+
+        return String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(currentTimer * 1000),
+                TimeUnit.MILLISECONDS.toSeconds(currentTimer * 1000) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(currentTimer * 1000))
+        );
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("tomodoro.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.initialize(primaryStage);
+
+        primaryStage.setTitle("Tomodoro");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
